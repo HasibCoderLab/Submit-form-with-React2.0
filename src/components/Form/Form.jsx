c48785892003e0b4d3dd3e60b75926be6ad1431e
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-21 22:41:29 +0600

    solve conditional Problem

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react'
 
 const Form = () => {
-    // =  =useState
+    // ========== useState ===============
+
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [pass, setPass] = useState("");
-    const  [error , setError] = useState("");
+    const [error, setError] = useState("");
 
     //   Function
+
     const handleSubmitForm = (e) => {
         e.preventDefault();
         if (pass.length < 9) {
-             setError("Password must be 9 char");
-        }else{
+            setError("Password must be 9 char");
+        } else {
             setError("");
             console.log(name);
             console.log(email);
             console.log(pass);
-            
-            
         }
     }
     return (
@@ -28,14 +28,14 @@ const Form = () => {
                 className="bg-sky-700 p-10 space-y-3 ">
                 <input onChange={(e) => setName(e.target.value)} type="text" placeholder="Name" name='text' required
                     className="block w-full p-2 border border-gray-300 " />
-                <input onChange={(e) => setEmail(e.target.value)} type="password" placeholder="Password" name='password' required
+                <input onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" name='password' required
                     className="block w-full p-2 border border-gray-300" />
-                <input onChange={(e) => setPass(e.target.value)} type="email" placeholder="Email " name='email' required
+                <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email " name='email' required
                     className="block w-full p-2 border border-gray-300 rounded" />
 
                 <input type="submit" value="submit"
                     className="block w-full p-2 border bg-blue-950 border-gray-300 rounded-3xl  cursor-pointer " />
-                    <p> {error} </p>
+                <p> {error} </p>
             </form>
 
         </div>
